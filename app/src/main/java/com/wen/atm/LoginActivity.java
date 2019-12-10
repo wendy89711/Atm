97528870bd24699b3edcb8eb81d3b94bdb0256b7
Wendy <wendy89711@gmail.com>
2019-12-10 14:28:08 +0800

    初始畫面

---
+++ app/src/main/java/com/wen/atm/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.wen.atm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void login(View view) {
+
+    }
+}
